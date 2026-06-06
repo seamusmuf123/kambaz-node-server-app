5651d3dbd1184ae09fef3afb854358f75c8572dc
seamusmuf123 <mufarinya.se@northeastern.edu>
2026-06-05 20:33:59 -0400

    Fix production session cookie

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ if (process.env.SERVER_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL
   };
 } else {
   sessionOptions.cookie = {
